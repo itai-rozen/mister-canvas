088cef6b9f1f9a3ec6ddeec040676564447123ea
Itai Rozen <itai.rozen@mail.huji.ac.il>
2019-02-13 15:01:45 +0200

    added canvas function on mouse move

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -1,6 +1,32 @@
 'use strict';
+var gCanvas = document.querySelector('#canvas');
+var gCtx = gCanvas.getContext('2d');
+var gIsBeginPath = false;
 
-function onMouseMovement(ev){
+
+function onMouseMovement(ev) {
     console.log(ev);
-    
+    var xAxis = document.querySelector('.mouse-x');
+    var yAxis = document.querySelector('.mouse-y');
+    var x = ev.offsetX;
+    var y = ev.offsetY;
+    xAxis.innerText = x;
+    yAxis.innerText = y;
+    if (gIsBeginPath) {
+        gCtx.lineTo(x, y);
+        gCtx.stroke();
+    }
+}
+
+function onTogglePath(ev) {
+    if (!gIsBeginPath) {
+        var x = ev.offsetX;
+        var y = ev.offsetY;
+        gCtx.beginPath();
+        gCtx.moveTo(x, y);
+        gIsBeginPath = true;
+    } else {
+        gIsBeginPath = false;
+        gCtx.closePath();
+    }
 }
